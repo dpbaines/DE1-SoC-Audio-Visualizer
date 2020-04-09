ea4b49bcd87d40c2e0c655e02dc440991446b13d
sophia-mazzonna <sophia.mazzonna@gmail.com>
2020-04-09 16:09:12 -0400

    tweak y_scale

--- main.c
+++ main.c
@@ -109,8 +109,8 @@ int main(void) {
 
          /*******************ANIMATION PART********************/
         clear_screen();
-	//array to store values
-	int y_values [256];
+	////array to store values
+	//int y_values [256];
 
 		for(int i = 0; i < 256; i++){
 
@@ -126,16 +126,16 @@ int main(void) {
 			y_plot = y_scale(value);
             draw_line(i+32, y_plot, i+32, 240, line_color);	
 			
-			//store y_plot values
-			y_values[i] = y_plot;
+			////store y_plot values
+			//y_values[i] = y_plot;
         } 
 	    
 	//connecting lines
-	for(int i = 0; i < (256-1); i++){
-		//int index_1 = i;
-		//int index_2 = i
-		draw_line(i+32, y_values[i], (i+1)+32, y_values[i+1], line_color);
-	}
+	//for(int i = 0; i < (256-1); i++){
+	//	//int index_1 = i;
+	//	//int index_2 = i
+	//	draw_line(i+32, y_values[i], (i+1)+32, y_values[i+1], line_color);
+	//}
 	    
         wait_for_vsync(); // swap front and back buffers on VGA vertical sync
         pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
@@ -451,13 +451,24 @@ double shittylog(double value) {
 	//return (y_value);
 //}
 
+//int y_scale(double y){	
+//	int y_value = 0;
+//	if(y < 200000000.0) y_value = 240;
+//	////else y_value = ((int)(240.0 - ((24.0)*(y/20000000000.0))));
+//	else y_value = (int)(240-(y/200000000.0));
+//	//else y_value = (int)(240.0 - ((sqrt(y))));///40000.0)));
+//	if (y_value < 0.0) y_value = 0.0; 
+//	return (y_value);
+//}
+
 int y_scale(double y){	
 	int y_value = 0;
-	if(y < 200000000.0) y_value = 240;
+	if(y < 1000000.0) y_value = 240;
+	else if(y_value > 240000000) y_value = 240.0;
 	////else y_value = ((int)(240.0 - ((24.0)*(y/20000000000.0))));
-	else y_value = (int)(240-(y/200000000.0));
+	else y_value = (int)(240-(y/1000000.0));
 	//else y_value = (int)(240.0 - ((sqrt(y))));///40000.0)));
-	if (y_value < 0.0) y_value = 0.0; 
+	if (y_value < 0.0) y_value = 240.0; 
 	return (y_value);
 }
 
